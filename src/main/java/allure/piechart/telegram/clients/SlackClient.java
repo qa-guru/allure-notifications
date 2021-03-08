dde012a610ed86cc8ac2ad7e539d2aef846a09d0
svasenkov <aanher@gmail.com>
2021-03-08 04:28:16 +0300

    added notifications to slack!

--- src/main/java/allure/piechart/telegram/clients/SlackClient.java
+++ src/main/java/allure/piechart/telegram/clients/SlackClient.java
@@ -1,22 +1,17 @@
 package allure.piechart.telegram.clients;
 
 import allure.piechart.telegram.chart.Chart;
-import allure.piechart.telegram.chart.ChartBuilder;
 import allure.piechart.telegram.models.Summary;
 import allure.piechart.telegram.options.OptionsValues;
 import io.restassured.response.ValidatableResponse;
-import org.knowm.xchart.BitmapEncoder;
-import org.knowm.xchart.PieChart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
 import java.io.File;
-import java.io.IOException;
 
 import static allure.piechart.telegram.chart.Chart.PIECHART_FILE_NAME;
 import static io.restassured.RestAssured.given;
-import static java.lang.String.format;
 
 /**
  * Клиент для отправки сообщений в slack
