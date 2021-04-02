8347d4ec1e08ee8495ebbc3224abe9cd9ee45cab
Ilya_Malykh <ferras2007@yandex.ru>
2021-04-02 21:15:54 +0300

    added html format

--- src/main/java/com/github/guru/qa/allure/notifications/message/Text.java
+++ src/main/java/com/github/guru/qa/allure/notifications/message/Text.java
@@ -12,11 +12,22 @@ import java.util.ResourceBundle;
  * @since 2.0.10
  */
 public class Text {
-    /** Возвращает отформатированное по шаблону сообщение. */
-    public static String formattedMessage() {
+    /** Возвращает отформатированное по шаблону сообщение в Markdown формате. */
+    public static String formattedMarkdownMessage() {
         ResourceBundle message = ResourceBundle.getBundle("template/text");
         MessageFormat formatter = new MessageFormat("");
-        formatter.applyPattern(message.getString("template"));
+        formatter.applyPattern(message.getString("markdown"));
         return formatter.format(TemplateUtil.templateData());
+    }
+
+    /** Возвращает отформатированное по шаблону сообщение в HTML формате. */
+    public static String formattedHtmlMessage() {
+        ResourceBundle message = ResourceBundle.getBundle("template/text");
+        MessageFormat formatter = new MessageFormat("");
+        formatter.applyPattern(message.getString("html"));
+        return formatter.format(TemplateUtil.templateData()).
+                replaceAll("bold", "<b>").
+                replaceAll("poly", "</b>").
+                replaceAll("nop", "<br>");
     }
 }
