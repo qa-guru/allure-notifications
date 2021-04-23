a33a5aec7d76a13b61c6ed0a44fe4a981d5847fb
AleksCh <ssss.utest@gmail.com>
2021-04-23 09:09:37 +0300

    add logo rendering

--- src/main/java/com/github/guru/qa/allure/notifications/chart/Chart.java
+++ src/main/java/com/github/guru/qa/allure/notifications/chart/Chart.java
@@ -5,6 +5,9 @@ import org.knowm.xchart.PieChart;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
@@ -14,6 +17,7 @@ import static org.knowm.xchart.BitmapEncoder.BitmapFormat.PNG;
 public class Chart {
     private static final Logger LOG = LoggerFactory.getLogger(Chart.class);
     private final static String FILE_NAME = "piechart";
+    private final static String LOGO_FILE_NAME = "logo";
 
     public static void createChart() {
         final String title = projectName();
@@ -32,15 +36,25 @@ public class Chart {
         List<int[]> colors = ChartSeries.addSeriesTo(chart);
         LOG.info("Add colors to series");
         ChartColors.addColorsTo(chart, colors);
-
         try {
             LOG.info("Try to save chart as {}.png", FILE_NAME);
             BitmapEncoder.saveBitmap(chart, FILE_NAME, PNG);
             LOG.info("Chart is created successfully");
+
         } catch (IOException e) {
             LOG.error("Error {} \n Reason {}", e.getLocalizedMessage(), e.getStackTrace());
             e.printStackTrace();
             System.exit(1);
+        }
+        try {
+            BufferedImage source = ImageIO.read(new File(FILE_NAME+".png"));
+            BufferedImage logo = ImageIO.read(new File(LOGO_FILE_NAME+".png"));
+
+            source.getGraphics().drawImage(logo, 3, 3, null);
+            File f = new File(FILE_NAME+".png");
+            ImageIO.write(source, "PNG", f);
+        } catch (Exception e) {
+            LOG.info("Logo file isn't existed");
         }
     }
 }
