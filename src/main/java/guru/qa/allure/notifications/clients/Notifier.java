305935a30594bc979e35e5152eb6b7658965a698
Efim Ilin <36734883+exmike@users.noreply.github.com>
2023-11-21 22:11:46 +0300

    Use Checkstyle for static code analysis (#232)

--- src/main/java/guru/qa/allure/notifications/clients/Notifier.java
+++ src/main/java/guru/qa/allure/notifications/clients/Notifier.java
@@ -3,6 +3,8 @@ package guru.qa.allure.notifications.clients;
 import guru.qa.allure.notifications.exceptions.MessagingException;
 
 public interface Notifier {
+
     void sendText() throws MessagingException;
+
     void sendPhoto(byte[] chartImage) throws MessagingException;
 }
