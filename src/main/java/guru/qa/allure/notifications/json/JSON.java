64b0d3107847f60fc0b6a4a3fb87ee1b9ddbbd84
Valery Yatsynovich <valery_yatsynovich@epam.com>
2023-11-22 12:21:08 +0200

    Improve error handling

--- src/main/java/guru/qa/allure/notifications/json/JSON.java
+++ src/main/java/guru/qa/allure/notifications/json/JSON.java
@@ -3,11 +3,16 @@ package guru.qa.allure.notifications.json;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
-import guru.qa.allure.notifications.exceptions.ConfigNotFoundException;
+import guru.qa.allure.notifications.util.ResourcesUtil;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
 
 /**
  * @author kadehar
@@ -16,14 +21,26 @@ import java.io.FileReader;
  */
 @Slf4j
 public class JSON {
+    private static final ResourcesUtil RESOURCES_UTIL = new ResourcesUtil();
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
-    public <T> T parse(String file, Class<T> clazz) {
-        log.info("Parsing *.json file by path {}", file);
-        try {
-            return GSON.fromJson(new FileReader(file), clazz);
-        } catch (FileNotFoundException ex) {
-            throw new ConfigNotFoundException("Unable to find config file by path " + file);
+    public <T> T parseFile(String file, Class<T> clazz) throws FileNotFoundException {
+        log.info("Mapping file at path {} to {} object", file, clazz.getSimpleName());
+        return GSON.fromJson(new FileReader(file), clazz);
+    }
+
+    public <T> T parseFile(File file, Class<T> clazz) throws FileNotFoundException {
+        log.info("Mapping file at path {} to {} object", file.getAbsolutePath(), clazz.getSimpleName());
+        return GSON.fromJson(new FileReader(file), clazz);
+    }
+
+    public <T> T parseResource(String resourcePath, Class<T> clazz) {
+        log.info("Mapping resource at path {} to {} object", resourcePath, clazz.getSimpleName());
+        try (InputStream inputStream = RESOURCES_UTIL.getResourceAsStream(resourcePath);
+             InputStreamReader inputStreamReader = new InputStreamReader(inputStream)) {
+            return GSON.fromJson(inputStreamReader, clazz);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 
--- src/main/java/guru/qa/allure/notifications/mapper/PhrasesMapper.java
+++ src/main/java/guru/qa/allure/notifications/mapper/PhrasesMapper.java
@@ -3,15 +3,12 @@ package guru.qa.allure.notifications.mapper;
 import guru.qa.allure.notifications.config.base.Base;
 import guru.qa.allure.notifications.json.JSON;
 import guru.qa.allure.notifications.model.phrases.Phrases;
-import guru.qa.allure.notifications.util.ResourcesUtil;
-import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author kadehar
  * @since 4.0
  * Utility class for mapping phrases *.json to Phrases object.
  */
-
-@Slf4j
 public class PhrasesMapper {
 
     private final Base base;
@@ -21,10 +18,7 @@ public class PhrasesMapper {
     }
 
     public Phrases map() {
-        String lang = base.getLanguage() + ".json";
-        String fullPath = new ResourcesUtil()
-                .resourcesPath("/phrases/" + lang);
-        log.info("Mapping {} to Phrases object", fullPath);
-        return new JSON().parse(fullPath, Phrases.class);
+        String phrasesJson = "/phrases/" + base.getLanguage() + ".json";
+        return new JSON().parseResource(phrasesJson, Phrases.class);
     }
 }
