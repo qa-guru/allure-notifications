27f99dcfb476a68fa3332c82ea46143e30c533e3
Roman Katkov <romangadle@gmail.com>
2025-07-17 17:25:45 +0700

    Logo downscaling logic was put in LogoScaler class

--- allure-notifications-api/src/main/java/guru/qa/allure/notifications/chart/Chart.java
+++ allure-notifications-api/src/main/java/guru/qa/allure/notifications/chart/Chart.java
@@ -3,6 +3,7 @@ package guru.qa.allure.notifications.chart;
 import guru.qa.allure.notifications.exceptions.MessageBuildException;
 import guru.qa.allure.notifications.model.legend.Legend;
 import guru.qa.allure.notifications.model.summary.Summary;
+import guru.qa.allure.notifications.scalers.LogoScaler;
 import lombok.extern.slf4j.Slf4j;
 import org.knowm.xchart.BitmapEncoder;
 import org.knowm.xchart.PieChart;
@@ -36,13 +37,9 @@ public class Chart {
 
         if (base.getLogo() != null) {
             try {
-                BufferedImage logo = ImageIO.read(new File(base.getLogo()));
-                double maxLogoWidth = chart.getWidth() * 0.23;
-                double maxLogoHeight = chart.getHeight() * 0.14;
-                if(logo.getWidth() > maxLogoWidth || logo.getHeight() > maxLogoHeight) {
-                    Image scaledLogo = scaleLogo(logo, chart);
-                    chartImage.getGraphics().drawImage(scaledLogo, 3, 3, null);
-                } else chartImage.getGraphics().drawImage(logo, 3, 3, null);
+                Image logo = LogoScaler.scaleLogo(
+                    ImageIO.read(new File(base.getLogo())), chart);
+                chartImage.getGraphics().drawImage(logo, 3, 3, null);
             } catch (Exception e) {
                 log.warn("Logo file isn't existed: " + base.getLogo());
             }
@@ -55,13 +52,5 @@ public class Chart {
         } catch (IOException e) {
             throw new MessageBuildException("Unable to create image with chart", e);
         }
-    }
-
-    private static Image scaleLogo(BufferedImage logo, PieChart chart) {
-            double scale = Math.min(chart.getWidth() * 0.23 / logo.getWidth(),
-                chart.getHeight() * 0.14 / logo.getHeight());
-            int newWidth = (int) (logo.getWidth() * scale);
-            int newHeight = (int) (logo.getHeight() * scale);
-            return logo.getScaledInstance(newWidth, newHeight, Image.SCALE_SMOOTH);
     }
 }
