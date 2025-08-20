8d06610200090721d278ce36d2d4cdfb7ab076c3
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2025-08-20 15:14:27 +0330

    Feat: Enhance photo sending functionality in Zoho Cliq client with JSON comments array

--- allure-notifications-api/src/main/java/guru/qa/allure/notifications/clients/cliq/CliqClient.java
+++ allure-notifications-api/src/main/java/guru/qa/allure/notifications/clients/cliq/CliqClient.java
@@ -19,6 +19,7 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
+import kong.unirest.json.JSONArray;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -64,11 +65,13 @@ public class CliqClient implements Notifier {
     public void sendPhoto(MessageData messageData, byte[] chartImage) throws MessagingException {
         String errorDescription = "Failed to post message with file to Zoho Cliq";
         try (CloseableHttpClient client = HttpClients.createDefault()) {
+            JSONArray commentsArray = new JSONArray();
+            commentsArray.put(MessageTemplate.createMessageFromTemplate(messageData, cliq.getTemplatePath()));
+            
             MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create()
                     .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
                     .addBinaryBody("files", chartImage, ContentType.IMAGE_PNG, "chart.png")
-                    .addTextBody("comments", 
-                            MessageTemplate.createMessageFromTemplate(messageData, cliq.getTemplatePath()));
+                    .addTextBody("comments", commentsArray.toString());
             
             HttpUriRequest request = RequestBuilder
                     .post(generateUrl("files"))
@@ -108,13 +111,19 @@ public class CliqClient implements Notifier {
             throws MessageSendException {
         try (CloseableHttpResponse responseBody = client.execute(request)) {
             int statusCode = responseBody.getStatusLine().getStatusCode();
-            String responseAsString = EntityUtils.toString(responseBody.getEntity());
-
-            if (statusCode != HttpStatus.SC_OK) {
-                throw new MessageSendException(
-                        String.format("%s. HTTP status code: %d, HTTP response: %s", errorDescription, statusCode,
-                                responseAsString));
+            
+            if (statusCode == HttpStatus.SC_OK || statusCode == HttpStatus.SC_NO_CONTENT) {
+                return;
             }
+            
+            String responseAsString = "";
+            if (responseBody.getEntity() != null) {
+                responseAsString = EntityUtils.toString(responseBody.getEntity());
+            }
+            
+            throw new MessageSendException(
+                    String.format("%s. HTTP status code: %d, HTTP response: %s", errorDescription, statusCode,
+                            responseAsString));
         } catch (IOException e) {
             throw new MessageSendException(errorDescription, e);
         }
