30775ed79333c932409fce55db34067ba6429b04
Valery Yatsynovich <valery_yatsynovich@epam.com>
2025-08-22 15:43:06 +0300

    Simplify chart building (#391)

--- allure-notifications-api/src/main/java/guru/qa/allure/notifications/chart/LogoScaler.java
+++ allure-notifications-api/src/main/java/guru/qa/allure/notifications/chart/LogoScaler.java
@@ -1,8 +1,7 @@
-package guru.qa.allure.notifications.scalers;
+package guru.qa.allure.notifications.chart;
 
 import guru.qa.allure.notifications.exceptions.InvalidArgumentException;
 import lombok.extern.slf4j.Slf4j;
-import org.knowm.xchart.PieChart;
 
 import java.awt.Image;
 import java.awt.image.BufferedImage;
@@ -17,18 +16,19 @@ public class LogoScaler {
     * Downscale logic is based on chart size, view and logo position, and available logo free area sizes.</p>
     *
     * @param logo the logo which should be downscaled
-    * @param chart the chart based on which logo should be downscaled
+    * @param chartWidth the width of the chart based on which logo should be downscaled
+    * @param chartHeight the width of the chart based on which logo should be downscaled
     * @return downscaled image with available width and height of logo
     * @throws InvalidArgumentException if logo or chart is null
     */
-    public static Image scaleLogo(BufferedImage logo, PieChart chart) {
-        if (logo == null || chart == null) {
-            throw new InvalidArgumentException("Logo or chart can't be null!");
+    public static Image scaleLogo(BufferedImage logo, int chartWidth, int chartHeight) {
+        if (logo == null) {
+            throw new InvalidArgumentException("Logo can't be null!");
         }
         // 23% is a max width of logo: between left edge of image and the top left edge of chart including 5px padding
-        double maxLogoWidth = chart.getWidth() * 0.23;
+        double maxLogoWidth = chartWidth * 0.23;
         // 14% is a max height of logo: between top left edge of chart and a top of image including 5px padding
-        double maxLogoHeight = chart.getHeight() * 0.14;
+        double maxLogoHeight = chartHeight * 0.14;
         if (logo.getWidth() > maxLogoWidth || logo.getHeight() > maxLogoHeight) {
             log.debug("Calculating scale: width {}, height {}", logo.getWidth(), logo.getHeight());
             double scale = Math.min(maxLogoWidth / logo.getWidth(), maxLogoHeight / logo.getHeight());
